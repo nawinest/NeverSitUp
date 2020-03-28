a516d6524c58bf3a883c3a2f1e4c5fc7559cffac
Nawin P <nawin.p@digi-cimb.com>
2020-03-28 15:26:48 +0700

    Add senior project

---
+++ Senior/neversitup_iOSInterview-master/neversitupTest/ViewController.swift
@@ -0,0 +1,169 @@
+//
+//  ViewController.swift
+//  neversitupTest
+//
+//  Created by Witawat wanamonthon on 30/1/20.
+//  Copyright © 2020 Witawat wanamonthon. All rights reserved.
+//
+
+import UIKit
+
+enum itemType {
+    case name
+    case price
+}
+
+class ViewController: UIViewController {
+
+    let itemList = [[itemType.name:"ปากกา",
+                     itemType.price:52],
+                    [itemType.name:"ตุ๊กตา",
+                     itemType.price:500],
+                    [itemType.name:"โคมไฟ",
+                     itemType.price:590],
+                    [itemType.name:"ชอคโกแลต",
+                     itemType.price:120],
+                    [itemType.name:"เตียง",
+                     itemType.price:59000],
+                    [itemType.name:"หนังสือ",
+                     itemType.price:250],
+                    [itemType.name:"แจกฟรี",
+                    itemType.price:0]]
+    
+    
+    @IBOutlet weak var numberOfItemsTextField: UITextField!
+    @IBOutlet weak var itemTextField: UITextField!
+    @IBOutlet weak var priceTextField: UITextField!
+    @IBOutlet weak var discountTextField: UITextField!
+    @IBOutlet weak var subTotalTextField: UITextField!
+    
+    var pickerView = UIPickerView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupTextFieldInput()
+    }
+
+    
+    func setupTextFieldInput(){
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        let toolBar = UIToolbar()
+        toolBar.barStyle = UIBarStyle.default
+        toolBar.isTranslucent = true
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(pressDone))
+        let cancelButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(pressCancel))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        itemTextField.inputAccessoryView = toolBar
+        itemTextField.inputView = pickerView
+        itemTextField.tintColor = .clear
+        itemTextField.placeholder = "กรุณาเลือกรายการสินค้า"
+        discountTextField.placeholder = "กรุณาระบุส่วนลด"
+        priceTextField.placeholder = "0"
+        priceTextField.isUserInteractionEnabled = false
+        priceTextField.delegate = self
+        discountTextField.delegate = self
+        numberOfItemsTextField.delegate = self
+        subTotalTextField.delegate = self
+        numberOfItemsTextField.placeholder = "กรุณาระบุจำนวน"
+        discountTextField.addTarget(self, action: #selector(discountTextFieldChange), for: .editingChanged)
+        numberOfItemsTextField.addTarget(self, action: #selector(numberOfTextFieldChange), for: .editingChanged)
+        subTotalTextField.addTarget(self, action: #selector(subTotalTextFieldChange), for: .editingChanged)
+    }
+    
+    
+    @objc func numberOfTextFieldChange (_ textField : UITextField){
+        checkTextFieldIsNegative(tfs: [numberOfItemsTextField])
+    }
+    
+    @objc func discountTextFieldChange (_ textField : UITextField){
+        checkTextFieldIsNegative(tfs: [discountTextField, subTotalTextField])
+    }
+    
+    @objc func subTotalTextFieldChange (_ textField : UITextField){
+        if let subTotalDouble = Float(textField.text!),
+            let price = Float(priceTextField.text!),
+            let numberOfItems = Float(numberOfItemsTextField.text!) {
+            let price = price * numberOfItems
+            let discount = 100 - ((subTotalDouble / price) * 100)
+            discountTextField.text = "\(discount)"
+            checkTextFieldIsNegative(tfs: [discountTextField, subTotalTextField])
+        }
+    }
+    
+    @objc func pressDone(){
+        itemTextField.resignFirstResponder()
+        let item = itemList[pickerView.selectedRow(inComponent: 0)]
+        if let price = item[.price] as? Int, let name = item[.name] as? String{
+            itemTextField.text = name
+            priceTextField.text = "\(price)"
+        }
+    }
+    
+    @objc func pressCancel(){
+        itemTextField.resignFirstResponder()
+    }
+    
+    
+    func calculateValue(){
+        if let price = Float(priceTextField.text!),
+            let discount = Float(discountTextField.text!),
+            let numberOfItems = Float(numberOfItemsTextField.text!) {
+                let price = price * numberOfItems
+                let priceCal = price - ( price * ( discount / 100 ) )
+                subTotalTextField.text = "\(priceCal)"
+                checkTextFieldIsNegative(tfs: [discountTextField, subTotalTextField, numberOfItemsTextField])
+        }
+    }
+    
+    private func checkTextFieldIsNegative(tfs: [UITextField]) {
+        tfs.forEach { (tf) in
+            tf.checkIsNegative()
+        }
+    }
+
+}
+
+extension ViewController : UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        calculateValue()
+    }
+}
+
+
+extension ViewController : UIPickerViewDelegate, UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return itemList.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return itemList[row][.name] as? String ?? ""
+    }
+}
+
+extension UITextField {
+    func checkIsNegative() {
+        if let textValue = self.text, let textDouble = Float(textValue) {
+            if textDouble < 0 {
+                self.textColor = .red
+            } else {
+                self.textColor = .black
+            }
+        }
+    }
+}
